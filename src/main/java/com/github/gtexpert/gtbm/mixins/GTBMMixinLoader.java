4f145c27b24a506a77af98d7863af7f32f271f4a
tier940 <tier940@protonmail.com>
2026-03-07 13:36:11 +0900

    Fix project

--- src/main/java/com/github/gtexpert/gtbm/mixins/GTBMMixinLoader.java
+++ src/main/java/com/github/gtexpert/gtbm/mixins/GTBMMixinLoader.java
@@ -1,11 +1,11 @@
 package com.github.gtexpert.gtbm.mixins;
 
-import java.util.AbstractMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
+import com.google.common.collect.ImmutableMap;
 
 import net.minecraftforge.fml.common.Loader;
 
@@ -15,13 +15,16 @@ import com.github.gtexpert.gtbm.api.util.Mods;
 
 import zone.rong.mixinbooter.ILateMixinLoader;
 
+@SuppressWarnings("unused")
 public class GTBMMixinLoader implements ILateMixinLoader {
 
-    public static final Map<String, Boolean> modMixinsConfig = Stream.of(
-            new AbstractMap.SimpleImmutableEntry<>(Mods.Names.GREGICALITY_MULTIBLOCKS,
-                    Mods.GregicalityMultiblocks.isModLoaded()))
-            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    public static final Map<String, Boolean> modMixinsConfig = new ImmutableMap.Builder<String, Boolean>()
+            .put(Mods.Names.GREGTECH, true)
+            .put(Mods.Names.GREGICALITY_MULTIBLOCKS, true)
+            .put(Mods.Names.FORESTRY, true)
+            .build();
 
+    @SuppressWarnings("SimplifyStreamApiCallChains")
     @Override
     public List<String> getMixinConfigs() {
         return modMixinsConfig.keySet().stream().map(mod -> "mixins." + ModValues.MODID + "." + mod + ".json")
@@ -32,28 +35,29 @@ public class GTBMMixinLoader implements ILateMixinLoader {
     public boolean shouldMixinConfigQueue(String mixinConfig) {
         String[] parts = mixinConfig.split("\\.");
 
-        if (parts.length != 4) {
+        if (parts.length != 4 && parts.length != 5) {
             ModLog.logger.fatal("Mixin Config Check Failed! Invalid Length.");
-            ModLog.logger.fatal("Mixin Config: " + mixinConfig);
+            ModLog.logger.fatal("Mixin Config: {}", mixinConfig);
             return true;
         }
 
         if (!Objects.equals(parts[1], ModValues.MODID)) {
-            ModLog.logger.error("Non GTExpertCore Mixin Found in Mixin Queue. This is probably an error. Skipping...");
-            ModLog.logger.error("Mixin Config: " + mixinConfig);
+            ModLog.logger.error("Non GTBeesMatrix Mixin Found in Mixin Queue. This is probably an error. Skipping...");
+            ModLog.logger.error("Mixin Config: {}", mixinConfig);
             return true;
         }
 
         if (!Loader.isModLoaded(parts[2])) {
-            ModLog.logger.error("Mod '" + parts[2] +
-                    "' is not loaded. If this is a normal GTExpertCore instance, this is probably an error.");
-            ModLog.logger.error("Not Loading Mixin Config " + mixinConfig);
+            ModLog.logger.error(
+                    "Mod '{}' is not loaded. If this is a normal GTBeesMatrix instance, this is probably an error.",
+                    parts[2]);
+            ModLog.logger.error("Not Loading Mixin Config {}", mixinConfig);
             return false;
         }
 
         if (!modMixinsConfig.containsKey(parts[2]) || !modMixinsConfig.get(parts[2])) {
-            ModLog.logger.info("Integration for Mod '" + parts[2] + "' is not enabled, or does not exist.");
-            ModLog.logger.info("Not Loading Mixin Config " + mixinConfig);
+            ModLog.logger.info("Integration for Mod '{}' is not enabled, or does not exist.", parts[2]);
+            ModLog.logger.info("Not Loading Mixin Config {}", mixinConfig);
             return false;
         }
 
